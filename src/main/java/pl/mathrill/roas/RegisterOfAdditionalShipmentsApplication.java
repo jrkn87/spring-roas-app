dd7e145a8ba651020fbbfdbc59e9abd5a0a863e7
JRK <jrkn87@gmail.com>
2020-08-29 22:19:57 +0200

    Init project.

---
+++ src/main/java/pl/mathrill/roas/RegisterOfAdditionalShipmentsApplication.java
@@ -0,0 +1,13 @@
+package pl.mathrill.roas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterOfAdditionalShipmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterOfAdditionalShipmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mathrill/roas/RegisterOfAdditionalShipmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mathrill.roas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterOfAdditionalShipmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
