6ba3f9653aad18d302badc56721d91e38b424ec9
JRK <jrkn87@gmail.com>
2020-08-29 22:45:27 +0200

    Adding a home endpoint.

--- src/main/java/pl/mathrill/roas/RegisterOfAdditionalShipmentsApplication.java
+++ src/main/java/pl/mathrill/roas/RegisterOfAdditionalShipmentsApplication.java
@@ -2,9 +2,17 @@ package pl.mathrill.roas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RegisterOfAdditionalShipmentsApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "building zone..";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterOfAdditionalShipmentsApplication.class, args);
